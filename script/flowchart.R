61d0e80dd914bd26b77f0cf8824882139f7daf51
Kisei R. Tanaka <kisei.tanaka@gmail.com>
2024-12-19 15:59:17 -1000

    Create flowchart.R

---
+++ script/flowchart.R
@@ -0,0 +1,27 @@
+install.packages("DiagrammeR")
+install.packages("DiagrammeRsvg")
+install.packages("rsvg")
+
+library(DiagrammeR)
+library(DiagrammeRsvg)
+library(rsvg)
+
+diagram_code <- "
+digraph G {
+  rankdir=TB;
+  node [shape=box, fontsize=10, fontname=Helvetica, style=filled, fillcolor=lightblue];
+
+  OccurrenceData    [label=\"Occurrence Data\\n(CRAG, NCRMP, OBIS)\"]
+  EnvVariables      [label=\"Environmental Variables\\n(Compile & Process Predictors)\"]
+  PrepareDataset    [label=\"Prepare & Integrate Dataset\\n(Combine Occurrences & Env. Data)\"]
+  RunMaxent         [label=\"Run Maxent\\n(Model Training)\"]
+  ModelOutput       [label=\"Model Output\\n(Suitability Maps, Var. Importance)\"]
+  ValidationNPS     [label=\"Validation with NPS\\n(Evaluate Predictions)\"]
+
+  OccurrenceData -> EnvVariables -> PrepareDataset -> RunMaxent -> ModelOutput -> ValidationNPS;
+}
+"
+
+g <- grViz(diagram_code)
+svg_code <- export_svg(g)
+rsvg_png(charToRaw(svg_code), file = "/Users/Kisei.Tanaka/Desktop/flowchart.png")
